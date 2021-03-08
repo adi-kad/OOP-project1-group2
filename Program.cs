e62c0fc87bd1e5e4f28bfc9419f7225ada2d0620
Rippan <jepperipnas@gmail.com>
2021-03-08 09:30:10 +0100

    First push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_project1_group2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
