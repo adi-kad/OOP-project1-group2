07b338ea472639edff6c2e30a2401c3673ad9dd3
Rippan <jepperipnas@gmail.com>
2021-03-10 11:25:47 +0100

    Employee class completed

--- Employee.cs
+++ Employee.cs
@@ -5,13 +5,15 @@ namespace OOP_project1_group2
 {
     class Employee : Person
     {
-        private List<int> activeBookings; // TBA
-        private bool trainingSchedule;
+        private int id;
 
-        public Employee(string name, string contactNumber) : base(name, contactNumber)
+        public Employee(int id, string name, string contactNumber) : base(name, contactNumber)
         {
-            trainingSchedule = false;
-            activeBookings = new List<int>();
+            this.id = id;
+        }
+        public string getEmployeeInfo()
+        {
+            return id + " " + name + " " + contactNumber;
         }
     }
 }
