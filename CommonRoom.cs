c40e235d788f3570178e1469dad1eb0cab566b2a
Adnan Kadrispahic <adikadrispahic@gmail.com>
2021-03-10 11:29:59 +0100

    CommonRoom class

---
+++ CommonRoom.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace OOP_project1_group2
+{
+    sealed class CommonRoom
+    {
+        private List<int> _trainingTools; //NYI - List should contain TrainingTool
+
+        public CommonRoom()
+        {
+            _trainingTools = new List<int>();
+        }
+
+        public void addTrainingTool()
+        {
+            
+        }
+
+        public void removeTrainingTool(int toolId)
+        {
+
+        }
+
+        public List<int> getTrainingTools()
+        {
+            return _trainingTools;
+        }
+
+        public void setTrainingToolStatus(int toolId)
+        {
+
+        }
+
+    }
+}
