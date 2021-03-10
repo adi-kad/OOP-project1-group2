040efda06a4809304068f76f7486c077703cd8d5
Martin Johansson <maxamartin1999@gmail.com>
2021-03-10 13:41:37 +0100

    updated facility.cs

--- Facility.cs
+++ Facility.cs
@@ -6,32 +6,32 @@ namespace OOP_project1_group2
     class Facility 
     {
         private int id;
-
-        //private List<Bookable> bookable = new List<Bookable>();
-
-        //private List<Employee> employees = new List<Employee>();
+        private List<Bookable> bookable = new List<Bookable>();
+        private List<Employee> employees = new List<Employee>();
         private List<Visitor> visitors = new List<Visitor>();
 
-        public Facility(int id, List<Visitor> visitors)
+        public Facility(int id, List<Bookable> bookable, List<Employee> employees, List<Visitor> visitors)
         {
             this.id = id;
+            this.bookable = bookable;
+            this.employees = employees;
             this.visitors = visitors;
         }
-        public void AddBookableRoom()
+        public void AddBookableRoom(int id, string type)
         {
-            /*
-            bookable.Add();
-            */
+            
+            bookable.Add(new BookableRoom(id, type));
+            
         }
         public void RemoveBookableRoom(int roomID)
         {
-            /*
-            bookable.Remove(roomID);
-            */
+            
+            bookable.RemoveAt(roomID);
+            
         }
-        /*public List<Bookable> GetBookableRooms()
+        public List<Bookable> GetBookableRooms()
         {
-            return bookable
-        }*/
+            return bookable;
+        }
     }
 }
