ae4e5bffd9b5fbf740fe464023b3fd4a0800b9c9
Martin Johansson <maxamartin1999@gmail.com>
2021-03-10 16:29:01 +0100

    Added trainer.cs

---
+++ Trainer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace OOP_project1_group2
+{
+    class Trainer : Employee
+    {
+        private Dictionary<string, bool> schedule;
+        public Trainer(Role role, int id, string name, string contactNumber) : base(id, name, contactNumber)
+        {
+            this.role = role;
+            this.schedule = new Dictionary<string, bool>();
+            schedule.Add("11:00-12:00", true);
+            schedule.Add("12:00-13:00", true);
+            schedule.Add("13:00-14:00", true);
+            schedule.Add("14:00-15:00", true);
+            schedule.Add("15:00-16:00", true);
+            schedule.Add("16:00-17:00", true);
+            schedule.Add("17:00-18:00", true);
+        }
+        public void ChangeScheduleFalse(string key)
+        {
+            schedule[key] = false;
+        }
+        public void ChangeScheduleTrue(string key)
+        {
+            schedule[key] = true;
+        }
+        public void PrintSchedule()
+        {
+            foreach (var n in schedule)
+            {
+                Console.WriteLine(n.Key + " " + n.Value);
+            }
+        }
+    }
+}
