0333407692e26dba986755dfb1b9ced4e37721df
Olof Atthem <72492246+Attben@users.noreply.github.com>
2021-03-10 13:47:11 +0100

    Update BookableRoom.cs

--- BookableRoom.cs
+++ BookableRoom.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace OOP_project1_group2
 {
-    class BookableRoom
+    class BookableRoom : Bookable
     {
         //Bookable times
         public BookableRoom(int id, string type) : base(id, type)
