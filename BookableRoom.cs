157eb0dfe993b7489ae7780b560a21068847de35
Attben <72492246+Attben@users.noreply.github.com>
2021-03-10 13:44:21 +0100

    Added code skeleton for BookableRoom.

---
+++ BookableRoom.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace OOP_project1_group2
+{
+    class BookableRoom
+    {
+        //Bookable times
+        public BookableRoom(int id, string type) : base(id, type)
+        {
+			
+        }          
+    }
+}
