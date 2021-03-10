e0fb596ab6b5e3a46b7046a5d73b0aa0bfb0f6a7
Rippan <jepperipnas@gmail.com>
2021-03-10 09:05:05 +0100

    Changes to Classdiagram

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP_project1_group2
 {
@@ -6,7 +7,26 @@ namespace OOP_project1_group2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            var dict = new Dictionary<string,bool>();
+            dict.Add("10:00-11:00", true);
+            dict.Add("11:00-12:00", true);
+            dict.Add("12:00-13:00", true);
+            dict.Add("13:00-14:00", true);
+            dict.Add("14:00-15:00", true);
+            dict.Add("15:00-16:00", true);
+            dict.Add("16:00-17:00", true);
+            dict.Add("17:00-18:00", true);
+
+            foreach (var d in dict)
+            {
+                Console.WriteLine(d.Key + " " + d.Value);
+            }
+
+            dict["10:00-11:00"] = false;
+            foreach (var d in dict)
+            {
+                Console.WriteLine(d.Key + " " + d.Value);
+            }
         }
     }
 }
