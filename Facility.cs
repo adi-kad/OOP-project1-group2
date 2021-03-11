715aebc65b29d390a3690b6e9ec0de4f99489fb3
Adnan Kadrispahic <adikadrispahic@gmail.com>
2021-03-11 14:16:16 +0100

    Changes to Facility.cs

--- Facility.cs
+++ Facility.cs
@@ -6,7 +6,7 @@ namespace OOP_project1_group2
     class Facility 
     {
         private int id;
-        private List<Bookable> bookable = new List<Bookable>();
+        private List<Bookable> bookable = new List<Bookable>();    
         private List<Employee> employees = new List<Employee>();
         private List<Visitor> visitors = new List<Visitor>();
 
@@ -37,5 +37,26 @@ namespace OOP_project1_group2
         {
             return bookable;
         }
+
+        public List<Dictionary<string, bool>> GetBookableHours()
+        {        
+            List<Dictionary<string, bool>> freeHours = new List<Dictionary<string, bool>>();               
+
+            foreach (var item in bookable)
+            {    
+                Dictionary<string, bool> temp = new Dictionary<string, bool>();        
+                foreach (var dict in ((BookableRoom)item).getSchedule())
+                {
+                   if (dict.Value == true)
+                   {
+                       temp.Add(dict.Key, dict.Value);
+                   }
+                }
+                freeHours.Add(temp);
+            }   
+            
+            return freeHours;       
+        }
+     
     }
 }
